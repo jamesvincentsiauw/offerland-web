ccff3fba9cc606f6c9245499d4bd63cf89abf07c
Vincent <jamesvincentsiauw@gmail.com>
2021-07-20 15:46:45 +0700

    add listing pages

--- app/routes.py
+++ app/routes.py
@@ -2,11 +2,15 @@ from flask import render_template, jsonify, abort
 from app import app
 from flask_paginate import Pagination, get_page_args
 import json
+import random
 
 f = open('app/data/listing.json')
 data = json.load(f)
+data = [d for d in data if d['status'] == 'Active']
+data = data[:100]
+# print(data)
 
-def get_data(offset=0, per_page=10):
+def get_data(offset=0, per_page=9):
     return data[offset: offset + per_page]
 
 def filterData(dic, listing_id):
@@ -22,7 +26,7 @@ def index():
     page, per_page, offset = get_page_args(page_parameter='page',
                                            per_page_parameter='per_page')
     per_page = 9
-    total = len(data)
+    total = 90
     pagination_data = get_data(offset=offset, per_page=per_page)
     pagination = Pagination(page=page, per_page=per_page, total=total,
                             css_framework='bootstrap4')
@@ -35,7 +39,9 @@ def index():
 @app.route('/detail/<listing_id>', methods=['GET'])
 def listing_page(listing_id):
     filtered_data = [d for d in data if filterData(d, listing_id)]
+    image = str(random.randint(1,9))
     if len(filtered_data) == 0:
         abort(404)
     return render_template('listing.html',
-                            data=filtered_data[0])
+                            data=filtered_data[0],
+                            image=image)
